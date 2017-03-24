8e38824170dfb81526bcc9d8bcf855751535558c
Antti Pilto <antti.pilto@student.oulu.fi>
2017-03-24 21:02:33 +0200

    reduced margins for mobile browsers

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,19 +12,19 @@ class Footer extends Component {
                     If you are a beginner, you might want to start by choosing the first two groups (a -&gt; ko) of hiragana. Once you manage to pass the last stage, add in one or two more groups. Aim for the perfection - you want to be answering quickly.</p>
                     <p>Have fun!</p>
 
-                    <h4>Update - 18 March 2017</h4>
-                    <p>* Bugfix: Fixed unintended behavior when same group was selected from both hiragana &amp; katakana. Thanks for the bug report!</p>
+                    <h4>Updates</h4>
+                    <p>
+                        <strong>24 March 2017</strong> &middot; Enhancement: Improved mobile browser usability with reduced margins.<br />
+                        <strong>18 March 2017</strong> &middot; Bugfix: Fixed unintended behavior when same group was selected from both hiragana &amp; katakana. Thanks for the bug report!
+                    </p>
                     <p>Coders needed! This app is open source, but I'm currently the only one working on it. If you'd like to practice your React.js skills,
                         feel free to check the issues and fork it on <a href="https://github.com/anzzstuff/kanaquiz">GitHub</a>!</p>
-
-                    <h4>Launch - 14 Aug 2016</h4>
                     <p>
-                        Here we are finally with a new version of Kana Quiz.
+                        <strong>14 Aug 2016</strong> &middot; Launch: Here we are finally with a new version of Kana Quiz.
                         This app doesn't require flash anymore, and should work great with mobile phones and tablets.
                         Note that this is still a beta version, so there may be a few quirks. I would love to hear
-                        your thoughts, so if you'd like, be sure to shoot me some feedback using {' '}
-                        <a href="https://goo.gl/forms/BwtgpkYjT1iyH8uE3">this form</a>! {' '}
-                        If you for some reason want to use the old version, it's still available <a href="http://simplemedia.org/kana/flash/">here</a>.
+                        your thoughts, so if you'd like, be sure to shoot me some <a href="https://goo.gl/forms/BwtgpkYjT1iyH8uE3">feedback</a>! {' '}
+                        If you for some reason want to use the old version, it's still available <a href="http://simplemedia.org/kana/flash/" rel="nofollow">here</a>.
                     </p>
                     <div className="copyright">&copy; <a href="http://simplemedia.org/">Antti Pilto</a> 2016</div>
                 </div>
